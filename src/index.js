20834ec79182117bade07a92a29c9b7a19bf21d2
Adrian Braga <adrian.braga.ro@gmail.com>
2022-12-04 20:58:12 -0400

    feat: Created middleware (checksTodoExists)

--- src/index.js
+++ src/index.js
@@ -33,8 +33,26 @@ function checksCreateTodosUserAvailability(request, response, next) {
 };
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(!validate(id)) return response.status(400).json({ message: 'Id Invalido' });
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) return response.status(404).json({ message: 'Usuario nao encontrado!' });
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+};
 
 function findUserById(request, response, next) {
   const { id } = request.params;
